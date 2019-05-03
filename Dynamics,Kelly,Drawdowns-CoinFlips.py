e20c67eec4207bf82efb9b6d34cc209b4571ef83
apollok1234 <50191385+apollok1234@users.noreply.github.com>
2019-05-03 12:08:59 -0700

    Add files via upload

--- Dynamics,Kelly,Drawdowns-CoinFlips.py
+++ Dynamics,Kelly,Drawdowns-CoinFlips.py
@@ -11,21 +11,23 @@ fraction = .2 #this is f, exposure of wealth/bankroll/reserves to gamble payoffs
 
 
 for j in range(1, paths):
-	Wealth = numpy.arange(0,trials,dtype=numpy.float) #initial wealth process sequence/array to be filled in
-	Wealth[0] = 1 #initial wealthj
-	
-	for i in range(1,trials):
-		if numpy.random.random() > 1.0 - probability_of_winning:
-			payoff = winning_payoff
-		else:
-			payoff = -1
-		Wealth[i] = Wealth[i-1]*(1+(fraction*payoff)) #recursive wealth process
-		
-	#print(Wealth)
-	print('final wealth is ', Wealth[trials-1])
-	matplotlib.pyplot.plot(Wealth) #plotting wealth process on arbitrary scale
-	geom_Avg = [(1/n)*math.log(Wealth[n]) for n in range(1,trials)] #sequence of running geometric averages
-	print('geometric average is ', geom_Avg[trials-2])
+    Wealth = numpy.arange(0,trials,dtype=numpy.float) #initial wealth process sequence/array to be filled in
+    Wealth[0] = 1 #initial wealthj
+    
+    for i in range(1,trials):
+        if numpy.random.random() > 1.0 - probability_of_winning:
+            payoff = winning_payoff
+        else:
+            payoff = -1
+        Wealth[i] = Wealth[i-1]*(1+(fraction*payoff)) #recursive wealth process
+        
+    #print(Wealth)
+    print('final wealth is ', Wealth[trials-1])
+    matplotlib.pyplot.plot(Wealth) #plotting wealth process on arbitrary scale
+    matplotlib.pyplot.title('Wealth/Bankroll/PortfolioValue for f = ' + str(fraction))
+    matplotlib.pyplot.xlabel('Number of Bets')
+    geom_Avg = [(1/n)*math.log(Wealth[n]) for n in range(1,trials)] #sequence of running geometric averages
+    print('geometric average is ', geom_Avg[trials-2])
 
 matplotlib.pyplot.show()
 print('theoretical long run growth rate is ', 0.6*math.log(1+fraction) + 0.4*math.log(1-fraction))
@@ -41,11 +43,11 @@ for j in range(0, len(f)):
     Wealth_f[0] = 1 #initial wealth
    
     for i in range(1,trials):
-    	if numpy.random.random() > 1.0 - probability_of_winning:
-    		payoff = winning_payoff
-    	else:
-    		payoff = -1
-    	Wealth_f[i] = Wealth_f[i-1]*(1+(f[j]*payoff)) #recursive wealth process
+        if numpy.random.random() > 1.0 - probability_of_winning:
+            payoff = winning_payoff
+        else:
+            payoff = -1
+        Wealth_f[i] = Wealth_f[i-1]*(1+(f[j]*payoff)) #recursive wealth process
     
     geomAvg_f = [(1/k)*math.log(Wealth_f[k]) for k in range(1,trials)] #sequence of running geometric averages
     geomAvgfor_f[j] = geomAvg_f[trials-2]
@@ -53,6 +55,8 @@ for j in range(0, len(f)):
 
 #matplotlib.pyplot.ylim((-.5,.5))
 matplotlib.pyplot.plot(f, geomAvgfor_f)    
+matplotlib.pyplot.title('LongTerm Growth Rate') 
+matplotlib.pyplot.xlabel('Exposure/Fraction f')
 matplotlib.pyplot.show()
 
 #3.we should add some plots of drawdowns and statistics on the drawdowns with fractional Kelly
@@ -82,18 +86,23 @@ for j in range(0, len(f[0:5])):
         if numpy.random.random() > 1.0 - probability_of_winning:
             payoff = winning_payoff
         else:
-        	payoff = -1
+            payoff = -1
         Wealth_f[i] = Wealth_f[i-1]*(1+(f[j]*payoff)) #recursive wealth process
         rel_drawdown_f[i] = (max(Wealth_f[0:i]) - Wealth_f[i]) / max(Wealth_f[0:i])
     
     #wealth process
     #print(Wealth_f)
     matplotlib.pyplot.figure()
+    #matplotlib.pyplot.subplot(2,1,1)
     matplotlib.pyplot.plot(Wealth_f)
+    matplotlib.pyplot.title('Wealth/PortfolioValue for f = ' + str(f[j]))
     
     #relative drawdown process
     #print(rel_drawdown_f)
     matplotlib.pyplot.figure()
+    #matplotlib.pyplot.subplot(2,1,2)
     matplotlib.pyplot.ylim((-1.0,1.0))
     matplotlib.pyplot.plot(rel_drawdown_f)
+    matplotlib.pyplot.title('RelativeDrawdown of Portfolio for Exposure f = ' + str(f[j]))
+
 #still need to add some statistics
